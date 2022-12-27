36a3d3ccb479a6fc339e5cae9fcfca392a301b94
Donald Johnson <donaldrjohnson66@gmail.com>
2022-12-26 23:30:01 -0600

    finished

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var gameNames = new List<string>() { "shooter", "rpg", "MMOs" };
+
+
+            IEnumerable<string> gameOrder = gameNames.OrderBy(x => x.Length).ThenBy(x => x);
+
+            foreach (string game in gameOrder)
+            {
+                Console.WriteLine(game);
+
+            }
+
+
+
+            
+
+            
         }
     }
 }
